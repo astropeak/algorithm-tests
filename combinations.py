50f5ed5dd9e72412943b1ac21de05a5408063540
Astropeak <astropeak@gmail.com>
2017-09-08 16:14:23 +0800

    Add some notes

--- combinations.py
+++ combinations.py
@@ -1,5 +1,11 @@
+# Note:
+#     以下实现为基本方法，使用递归的方法。缺点是这样会涉及很多重复计算。
+#     优化方法为：可以采用动态规划，以(idx, target)为key, 将对应结果保存下来，则下次再需要这个结果时，
+# 可以直接返回，而不是重新计算一遍。
 def combinations(numbers, idx, target, result, current=[]):
-    # print(numbers, idx, target, current)
+    '''
+    Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T. Each number in C may only be used once in the combination.
+    '''
     if target == 0:
         result.append(current)
         return
@@ -12,8 +18,8 @@ def combinations(numbers, idx, target, result, current=[]):
     cur.append(numbers[idx])
     combinations(numbers, idx+1, target-numbers[idx], result, cur)
 
-    # 2. Not use this element. In this cae, we should skip all the same elements 
-    # to avoid duplicates
+    # 2. Not use this element.
+    # In this case, we should skip all the same elements to avoid duplicates
     for i in range(idx+1, len(numbers)):
         if numbers[i] != numbers[idx]:
             combinations(numbers, i, target, result, current)
